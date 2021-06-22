0f96eb2e64f3687ab2378fa1ac1898f001a38f9e
Rody van der Jagt <rodyvanderj8@gmail.com>
2021-06-22 15:04:59 +0200

    Add body-parser

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+
 app.get('', (req, res) => {
     res.send("Hello, World");
 })
 
+const envelopesRouter = require('./server/envelopes.js');
+app.use('/envelopes', envelopesRouter);
 
 const PORT = process.env.PORT || 3000;
 
