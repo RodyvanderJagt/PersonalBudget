5165c7bcbc77aba85f45a182cef58e32f1994add
Rody van der Jagt <rodyvanderj8@gmail.com>
2021-06-22 14:24:08 +0200

    change port to 3000 and add simple endpoint

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 
+app.get('', (req, res) => {
+    res.send("Hello, World");
+})
 
 
-const PORT = process.env.PORT || 4001;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
 
