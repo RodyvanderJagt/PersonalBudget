4dc85f0959bdea86cf320c864ecd3a94ce22b9bc
Rody van der Jagt <rodyvanderj8@gmail.com>
2021-07-06 13:06:17 +0200

    Update server.js to eliminate outdated bodyparser

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
-
-app.get('', (req, res) => {
-    res.send("Hello, World");
-})
+app.use(express.json())
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+)
 
 const envelopesRouter = require('./server/envelopes.js');
 app.use('/envelopes', envelopesRouter);
